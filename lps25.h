64db1704bcf1519df2bd3256decf6f4779632f4d
Enrico Rossi <e.rossi@tecnobrain.com>
2016-01-13 20:38:38 +0100

    add lps25 headers

---
+++ lps25.h
@@ -0,0 +1,55 @@
+/*
+    LPS25H STMicroelectronics MEMS pressure sensor.
+    Copyright (C) 2016 Enrico Rossi
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 3 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+*/
+
+#ifndef _LPS25_h_
+#define _LPS25_h_
+
+/*
+ * addr = 0b101110x[r/w]
+ * x = SA0 pin
+ */
+
+#define LPS25_ADDR 0xb8
+
+/* Registers */
+#define LPS25_R_REF_P_XL 0x08
+#define LPS25_R_REF_P_L 0x09
+#define LPS25_R_REF_P_H 0x0A
+#define LPS25_R_WHO_AM_I 0x0F
+#define LPS25_R_RES_CONF 0x10
+#define LPS25_R_CTRL_REG1 0x20
+#define LPS25_R_CTRL_REG2 0x21
+#define LPS25_R_CTRL_REG3 0x22
+#define LPS25_R_CTRL_REG4 0x23
+#define LPS25_R_INTERRUPT_CFG 0x24
+#define LPS25_R_INT_SOURCE 0x25
+#define LPS25_R_STATUS_REG 0x27
+#define LPS25_R_PRESS_OUT_XL 0x28
+#define LPS25_R_PRESS_OUT_L 0x29
+#define LPS25_R_PRESS_OUT_H 0x2A
+#define LPS25_R_TEMP_OUT_L 0x2B
+#define LPS25_R_TEMP_OUT_H 0x2C
+#define LPS25_R_FIFO_CTRL 0x2E
+#define LPS25_R_FIFO_STATUS 0x2F
+#define LPS25_R_THS_P_L 0x30
+#define LPS25_R_THS_P_H 0x31
+#define LPS25_R_RPDS_L 0x39
+#define LPS25_R_RPDS_H 0x3A
+
+#endif
